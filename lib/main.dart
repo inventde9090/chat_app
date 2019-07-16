a4d2559479e96b8c1ec575e08bda38523ac08b9c
Drink Hub <drinkhub9090@gmail.com>
2019-07-16 18:54:25 +0100

    Project Setup

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+
+class MyApp extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Hello World'),
+        ),
+        body: Card(
+          child: Column(
+            children: <Widget>[
+              Text('This is a sample test'),
+              Text('Another text'),
+              Text('last text')
+              
+            ],),),
+      )
+    );
+  }
+}
